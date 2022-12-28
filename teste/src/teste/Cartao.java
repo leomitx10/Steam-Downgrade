d5d8e5972c35733fc7a83333ab823cb1f784d678
Gustavo França <61592832+gustavofbs@users.noreply.github.com>
2022-12-28 10:53:44 -0300

    Update

--- teste/src/teste/Cartao.java
+++ teste/src/teste/Cartao.java
@@ -2,10 +2,41 @@ package teste;
 
 public class Cartao {
     
-    // Atributos
     private String nome;
     private String bandeira;
     private String numero;
     private String validade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getBandeira() {
+        return bandeira;
+    }
+
+    public void setBandeira(String bandeira) {
+        this.bandeira = bandeira;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
     
 }
