ef7bd747470cf92f913323cc84f043ec9e32fe5c
Gustavo França <61592832+gustavofbs@users.noreply.github.com>
2022-12-28 10:59:16 -0300

    Update Social.java

--- teste/src/teste/Social.java
+++ teste/src/teste/Social.java
@@ -2,9 +2,32 @@ package teste;
 
 public class Social {
     
-    // Atributos
     private int qtdAmigos;
     private String amigos;
     private String convites;
+
+    public int getQtdAmigos() {
+        return qtdAmigos;
+    }
+
+    public void setQtdAmigos(int qtdAmigos) {
+        this.qtdAmigos = qtdAmigos;
+    }
+
+    public String getAmigos() {
+        return amigos;
+    }
+
+    public void setAmigos(String amigos) {
+        this.amigos = amigos;
+    }
+
+    public String getConvites() {
+        return convites;
+    }
+
+    public void setConvites(String convites) {
+        this.convites = convites;
+    }
     
 }
