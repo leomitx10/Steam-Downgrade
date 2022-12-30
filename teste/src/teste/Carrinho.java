340e0d7007d2935fd317095caa37ab552b2055c9
Gustavo França <61592832+gustavofbs@users.noreply.github.com>
2022-12-30 19:36:32 -0300

    Update Carrinho.java

--- teste/src/teste/Carrinho.java
+++ teste/src/teste/Carrinho.java
@@ -3,10 +3,7 @@ import java.util.ArrayList;
 public class Carrinho extends ContaJogos {
     private ArrayList<Jogo> Jogo;
     private Jogo game;
-    private Pedido pedido
     
-    
-
     //construtor
 
     public Carrinho(ArrayList<Jogo> Jogo, Jogo game, int quantidadeJogos) {
@@ -31,17 +28,7 @@ public class Carrinho extends ContaJogos {
     public void setGame(Jogo game) {
         this.game = game;
     }
-    
-    public Pedido getPedido() {
-        return pedido;
-    }
 
-    public void setPedido(Pedido pedido) {
-        this.pedido = pedido;
-    }
-
-    
-  
     //metodos
     public void removerCarrinho(){
     }
