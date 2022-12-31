6ccbbbd25a644745d6293a933ff0fc5de00662dc
Leandro de Almeida Oliveira <90487905+leomitx10@users.noreply.github.com>
2022-12-30 19:22:37 -0500

    interface-grafica2

---
+++ Steam_Downgrade/src/teste/Boleto.java
@@ -0,0 +1,29 @@
+package teste;
+import java.util.Date;
+public class Boleto extends Formadepagamento{
+    private String codigo;
+    private Date dataVencimento;
+
+    public Boleto(Pagamento pagamento) {
+        super(pagamento);
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+    
+    
+    
+}
