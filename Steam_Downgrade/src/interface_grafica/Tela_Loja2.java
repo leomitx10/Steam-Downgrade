afe77e9085db3a8aa070c0e5554020814a02602d
leandro <leandroalmeidaoliveira650@gmail.com>
2023-01-20 01:29:26 -0300

    addimagemfunc

--- Steam_Downgrade/src/interface_grafica/Tela_Loja2.java
+++ Steam_Downgrade/src/interface_grafica/Tela_Loja2.java
@@ -63,6 +63,8 @@ public class Tela_Loja2 extends javax.swing.JFrame {
         ImagemDAO dao5 = new ImagemDAO();
         Imagem exe5 = dao5.buscar(id5);
         ManipularImagem.exibiImagemLabel(exe5.getImagem(), labelimagem6); 
+        
+        
     }
 
     /**
@@ -116,7 +118,6 @@ public class Tela_Loja2 extends javax.swing.JFrame {
         jLabel21 = new javax.swing.JLabel();
         jButton24 = new javax.swing.JButton();
         jButton25 = new javax.swing.JButton();
-        jLabel8 = new javax.swing.JLabel();
         lblTitulo7 = new javax.swing.JLabel();
         labelimagem1 = new javax.swing.JLabel();
         labelimagem2 = new javax.swing.JLabel();
@@ -124,6 +125,7 @@ public class Tela_Loja2 extends javax.swing.JFrame {
         labelimagem4 = new javax.swing.JLabel();
         labelimagem5 = new javax.swing.JLabel();
         labelimagem6 = new javax.swing.JLabel();
+        labelimagem7 = new javax.swing.JLabel();
         btnDeslogar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -382,10 +384,6 @@ public class Tela_Loja2 extends javax.swing.JFrame {
             }
         });
 
-        jLabel8.setFont(new java.awt.Font("Liberation Sans", 0, 20)); // NOI18N
-        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/eldenRingSmall_150.png"))); // NOI18N
-        jLabel8.setMinimumSize(new java.awt.Dimension(500, 300));
-
         lblTitulo7.setFont(new java.awt.Font("Liberation Sans", 1, 18)); // NOI18N
         lblTitulo7.setText("-------");
 
@@ -419,6 +417,11 @@ public class Tela_Loja2 extends javax.swing.JFrame {
         labelimagem6.setMinimumSize(new java.awt.Dimension(150, 150));
         labelimagem6.setPreferredSize(new java.awt.Dimension(150, 150));
 
+        labelimagem7.setText("jLabel1");
+        labelimagem7.setMaximumSize(new java.awt.Dimension(150, 150));
+        labelimagem7.setMinimumSize(new java.awt.Dimension(150, 150));
+        labelimagem7.setPreferredSize(new java.awt.Dimension(150, 150));
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -448,7 +451,8 @@ public class Tela_Loja2 extends javax.swing.JFrame {
                                             .addComponent(lblValor5, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
                                             .addComponent(lblTitulo5, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                     .addGroup(jPanel1Layout.createSequentialGroup()
-                                        .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(labelimagem7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                         .addGap(12, 12, 12)
                                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -577,7 +581,7 @@ public class Tela_Loja2 extends javax.swing.JFrame {
                             .addComponent(jButton22)
                             .addComponent(jButton25)))
                     .addComponent(jLabel21, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(labelimagem7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(30, Short.MAX_VALUE))
         );
 
@@ -901,7 +905,6 @@ public class Tela_Loja2 extends javax.swing.JFrame {
     private javax.swing.JButton jButton9;
     private javax.swing.JDesktopPane jDesktopPane1;
     private javax.swing.JLabel jLabel21;
-    private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
@@ -911,6 +914,7 @@ public class Tela_Loja2 extends javax.swing.JFrame {
     private javax.swing.JLabel labelimagem4;
     private javax.swing.JLabel labelimagem5;
     private javax.swing.JLabel labelimagem6;
+    private javax.swing.JLabel labelimagem7;
     public javax.swing.JLabel lblTitulo1;
     private javax.swing.JLabel lblTitulo2;
     private javax.swing.JLabel lblTitulo3;
@@ -929,7 +933,7 @@ public class Tela_Loja2 extends javax.swing.JFrame {
     private javax.swing.JLabel lblValor8;
     // End of variables declaration//GEN-END:variables
 private void mostrarjogo() {
-        try {
+        
             JogoDAO objjogo = new JogoDAO();
             
             ArrayList<Jogo> lista = objjogo.listJogos();
@@ -953,11 +957,27 @@ private void mostrarjogo() {
             lblTitulo6.setText(lista.get(5).getNome());
             lblValor6.setText("R$ "+Float.toString(lista.get(5).getValor()));
             
-            if(lista.size()==7){}
+            if(lista.size()==7){
+                lblTitulo7.setText(lista.get(6).getNome());
+                lblValor7.setText("R$ "+Float.toString(lista.get(6).getValor()));
+                
+                int id6 = 7;
+                ImagemDAO dao6 = new ImagemDAO();
+                Imagem exe6 = dao6.buscar(id6);
+                ManipularImagem.exibiImagemLabel(exe6.getImagem(), labelimagem7); 
+            }else{
+                 lblTitulo7.setText("");
+                 lblValor7.setText("");
+                 jButton24.setVisible(false);
+                 jButton25.setVisible(false);
+                 labelimagem7.setText("");
+            }
+            
+            
+            
+            
         
-        } catch(Exception error) {
-            System.out.println("Error: " + error);
-        }
+      
     }
 
    
